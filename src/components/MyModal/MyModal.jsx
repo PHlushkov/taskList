1bd96f8e7460ebf8127ed6c04aeed61fc7e3eeb9
Pavel <pavelgluskov264@gmail.com>
2023-11-02 10:04:09 +0200

    made the title an input line

--- src/components/MyModal/MyModal.jsx
+++ src/components/MyModal/MyModal.jsx
@@ -38,6 +38,7 @@ function MyModal({setTasks}) {
   const timeEnd = useSelector(state => state.time.timeEnd)
   const openModal = useSelector(state => state.modal.modal)
 
+  const [taskTitle, setTaskTitle] = useState("");
   const [taskComment, setTaskComment] = useState("");
   const [taskDate, setTaskDate] = useState(dayjs())
   const [color, setColor] = useState("#8641E6")
@@ -49,6 +50,7 @@ const saveTask = () => {
   const taskData = {
     id: uuidv4(),
     time: timeStart.toISOString(),
+    title: taskTitle,
     comment: taskComment,
     date: taskDate.toISOString(),
     backgroundColor: color,
@@ -59,6 +61,7 @@ const saveTask = () => {
   savedTasks.push(taskData);
   localStorage.setItem("tasks", JSON.stringify(savedTasks));
   setTaskComment("")
+  setTaskTitle("")
   dispatch({type: "CLOSE MODAL", payload: false})
   setTasks((prevTasks) => [...prevTasks, taskData])
   dispatch({type: "CHANGE TIME START", payload: dayjs()})
@@ -78,7 +81,13 @@ const saveTask = () => {
         <Box sx={style}>
             <div className="my-modal__body">
                 <div className="my-modal__header">
-                    <span className="my-modal__title">Add Title</span>
+                    <input 
+                      className='my-modal__title' 
+                      placeholder='Add Title' 
+                      type="text" 
+                      value={taskTitle}
+                      onChange={(e) => setTaskTitle(e.target.value)}
+                    />
                     <input className='my-modal__color' value={color} onChange={(e)=>setColor(e.target.value)} type="color" />
                 </div>
                 <div className="my-modal__date">
